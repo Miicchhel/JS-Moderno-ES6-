c0b9576f690707d1f278ffe7a40587eb33b5e8a5
michel <michel.ferreira.melo@gmail.com>
2022-04-05 11:44:40 -0300

    Aula 01 - Interpolação de string

---
+++ aulas/scripts/01-Interpolacao-de-String.js
@@ -0,0 +1,8 @@
+let spaceshipName = "Colossus"
+let velocity = prompt(`Digite a velocidade da nave ${spaceshipName} em km/s`)
+
+//concatenação de strings
+console.log("Nome da nave: " + spaceshipName + "\nsua velocidade atual é: " + velocity +"km/s")
+
+//interpolação de strings
+console.log(`Nome da nave: ${spaceshipName} \nsua velocidade atual é: ${velocity}km/s`)
